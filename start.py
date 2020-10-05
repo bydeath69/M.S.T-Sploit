4b4f77ab6bcca36a3776c3861d9a9e5979b077b9
bydeath69 <69207642+bydeath69@users.noreply.github.com>
2020-10-06 01:03:31 +0400

    Create start.py

---
+++ start.py
@@ -0,0 +1,30 @@
+import os                                                                                                                                    import sys
+import time
+                                                                                                                                             os.system("clear")
+banner="""
+\tCode: BY DEATH69\033[31;40;1m
+                   ─────█─▄▀█──█▀▄─█─────
+                   ────▐▌──────────▐▌────                                                                                                                       ────█▌▀▄──▄▄──▄▀▐█────                                                                                                                       ───▐██──▀▀──▀▀──██▌───
+                   ──▄████▄──▐▌──▄████▄──
+      \33[35m     ☠         Code  : bydeath69         ☠
+         \33[33m            Youtube : BY DEATH EDIT     ☠
+        \33[32m            İnstagram : @bydeath69_x      ☠
+        \33[36m   ☾★ ☾★ Milli Cyber-Team ☾★ ☾★
+          \33[39m[1]Dos ddos saldirisi
+          \33[39m[2]Port Tarayıcı
+          \33[39m[3]Trojan Aracı
+          \33[39m[4]Tool x Benzeri
+          \33[39m[5]Arama spamı
+          \33[39m[0]Termuxu guncelle
+                                            \33[31m
+"""
+print (banner)
+islem=int(input("Işlem Numarası: "))
+
+if islem==1:os.system("python2 girme.py")
+elif islem==2:os.system("python2 burayadokunma.py")
+elif islem==3:os.system("python burayadokunmayin.py")
+elif islem==4:os.system("python .py")
+elif islem==5:os.system("bash ..sh")
+elif islem==0:os.system("pkg install && pkg install python && pkg install python2 && pkg install python3 && pkg insta                       >
+else:os.system("python start.py")
