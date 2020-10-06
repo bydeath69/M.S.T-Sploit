0cb80d3de8d5d642cc55e759bf99874f841ae1ce
bydeath69 <69207642+bydeath69@users.noreply.github.com>
2020-10-06 12:10:55 +0400

    Update start.py

--- start.py
+++ start.py
@@ -4,15 +4,15 @@ import time
 
 os.system("clear")
 banner="""
-\tCode: BY DEATH69\033[31;40;1m
+\tCode: Web Attack Birimi\033[31;40;1m
                    ─────█─▄▀█──█▀▄─█─────
                    ────▐▌──────────▐▌────
                    ────█▌▀▄──▄▄──▄▀▐█────
                    ───▐██──▀▀──▀▀──██▌───
                    ──▄████▄──▐▌──▄████▄──
-      \33[35m     ☠         Code  : bydeath69         ☠
-         \33[33m            Youtube : BY DEATH EDIT     ☠
-        \33[32m            İnstagram : @bydeath69_x      ☠
+      \33[35m     ☠         Code :m.s.t web birim komutan yardimcisi bydeath69 ☠
+         \33[33m            Youtube : Milli Cyber-Team    ☠
+        \33[32m            İnstagram : @Millicyberteam.com.tr     ☠
         \33[36m   ☾★ ☾★ Milli Cyber-Team ☾★ ☾★
           \33[39m[1]Dos ddos saldirisi
           \33[39m[2]Port Tarayıcı
@@ -30,5 +30,5 @@ elif islem==2:os.system("python2 ....py")
 elif islem==3:os.system("python2 ...py")
 elif islem==4:os.system("python .py")
 elif islem==5:os.system("bash ..sh")                                                                                             
-elif islem==0:os.system("pkg install && pkg install python && pkg install python2 && pkg install python3")
+elif islem==0:os.system("pkg install && pkg install python && pkg install python2 && pkg install python3 && pkg install bs4")
 else:os.system("python start.py")
